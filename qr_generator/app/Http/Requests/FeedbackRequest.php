85ca9b10fb38d9fab4093269957da60e5143d1df
pavel <antonov.pavel.00@yandex.ru>
2023-09-12 15:45:08 +0300

    fix validation erros

--- qr_generator/app/Http/Requests/FeedbackRequest.php
+++ qr_generator/app/Http/Requests/FeedbackRequest.php
@@ -18,10 +18,10 @@ class FeedbackRequest extends FormRequest implements RequestInterface
     {
         return [
             'rating' => 'required|integer|min:0|max:5',
-            'feedback_text' => 'nullable|alpha_num|max:255|min:0',
+            'feedback_text' => 'nullable|string|max:255|min:0',
             'is_contact' => 'nullable',
-            'name' => 'nullable|required_with_all:is_contact|alpha_num|max:255|min:2',
-            'contact' => 'nullable|required_with_all:is_contact'
+            'name' => 'nullable|required_with_all:is_contact|string|max:255|min:2',
+            'contact' => 'nullable|required_with_all:is_contact|regex:/^\s?(\+\s?7|8)([- ()]*\d){10}$/'
         ];
     }
 
@@ -41,13 +41,14 @@ class FeedbackRequest extends FormRequest implements RequestInterface
             'rating.required' => 'Поле обязательно к заполнению',
             'rating.max' => 'Максимальная возможная оценка 5',
             'rating.min' => 'Минимальная возможная оценка 0',
-            'feedback_text.alpha_num' => 'Поле не должно содержать спецсимволов',
+            'feedback_text.string' => 'Поле не должно содержать спецсимволов',
             'feedback_text.max' => 'Макисмальное количество символов 255',
             'name.required_with_all' => 'Поле обязательно к заполнению',
-            'name.alpha_num' => 'Поле не должно содержать спецсимволов',
+            'name.string' => 'Поле не должно содержать спецсимволов',
             'name.max' => 'Макисмальное количество символов 255',
             'name.min' => 'Минимальное количество символов 2',
             'contact.required_with_all' => 'Поле обязательно к заполнению',
+            'contact.regex' => 'Поле не соответствует формату номера телефона',
         ];
     }
 
